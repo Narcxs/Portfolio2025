6182cf125bf24137daf76015699b1c016ce66e41
Narcxs <simeon.morello11@gmail.com>
2025-01-07 20:03:34 +0300

    fixing home menu

--- src/components/Menu/HomeItems/StacksList.jsx
+++ src/components/Menu/HomeItems/StacksList.jsx
@@ -23,19 +23,17 @@ export default function StacksList() {
   ];
   return (
     <>
-      <h2 className="mb-8 text-2xl font-medium">My Stack</h2>
-      <div className="flex items-center gap-6 flex-wrap">
+      <h2 className="text-2xl font-medium mb-8">My Stack</h2>
+
+      <div className="flex flex-wrap gap-6">
         {stacks.map((stack, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-x-4 w-[193px] cursor-pointer"
-          >
-            <div className="w-[72px] h-[72px] flex items-center justify-center border-dashed border-[1px] rounded-lg border-gray-600 p-2 box-border hover:border-white50  transition duration-500">
+          <div key={index} className="flex items-center gap-x-4 w-[220px]">
+            <div className="min-w-[72px] min-h-[72px] w-[72px] h-[72px] flex items-center justify-center border border-dashed border-gray-600 rounded-lg p-2">
               {stack.logo ? (
                 <img
                   src={stack.logo}
-                  className="w-full h-full object-contain"
                   alt={`${stack.stack} logo`}
+                  className="w-[48px] h-[48px] object-contain"
                 />
               ) : (
                 <span className="text-xs text-gray-500">No logo</span>
@@ -43,10 +41,10 @@ export default function StacksList() {
             </div>
 
             <div className="flex flex-col">
-              <h4 className="font-semibold text-white text-sm">
+              <h4 className="text-sm font-semibold text-white">
                 {stack.stack}
               </h4>
-              <h6 className="text-gray-400 text-xs">{stack.subtitle}</h6>
+              <p className="text-xs text-gray-400">{stack.subtitle}</p>
             </div>
           </div>
         ))}
