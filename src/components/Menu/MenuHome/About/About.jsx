c4a40277134ccd7f36b2c6534b375d0348f7629a
Narcxs <simeon.morello11@gmail.com>
2025-01-09 16:09:56 +0300

    about

--- src/components/Menu/MenuHome/About/About.jsx
+++ src/components/Menu/MenuHome/About/About.jsx
@@ -1,3 +1,34 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div>
+        <h1 className="font-semibold text-5xl mb-8 ">About Me</h1>
+        <div className="pb-8 border-b-[1px] border-dashed border-gray-600 mb-8">
+          <button className="px-4 py-3 bg-white50 rounded-lg text-black950 font-semibold text-sm">
+            Get In Touch
+          </button>
+        </div>
+      </div>
+      <div className="w-full max-w-2xl bg-black900 rounded-lg p-6 flex justify-between items-center">
+        <div className="flex flex-col gap-2 text-center flex-1 -ml-4">
+          <span className="text-white text-xl font-medium">Based In</span>
+          <span className="text-gray-400">Madagascar</span>
+        </div>
+
+        <div className="h-12 w-px bg-gray-600" />
+
+        <div className="flex flex-col gap-2 text-center flex-1">
+          <span className="text-white text-xl font-bold">+2</span>
+          <span className="text-gray-400">Years of Experience</span>
+        </div>
+
+        <div className="h-12 w-px bg-gray-600" />
+
+        <div className="flex flex-col gap-2 text-center flex-1">
+          <span className="text-white text-xl font-bold">2</span>
+          <span className="text-gray-400">Projects Completed</span>
+        </div>
+      </div>
+    </>
+  );
 }
